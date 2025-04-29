830b512b28245cb4505cf4c5ecb404377f157347
Vidar Normann <1858786+vidarno@users.noreply.github.com>
2025-04-29 21:56:28 +0200

    Readinessprobe endpoint on healthz (#15)

--- main.go
+++ main.go
@@ -22,6 +22,16 @@ func main() {
 	http.HandleFunc("/records", webhook.Records)
 	http.HandleFunc("/adjustendpoints", webhook.AdjustEndpoints)
 
+	// Add the /healthz endpoint
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+	            w.WriteHeader(http.StatusMethodNotAllowed)
+	            return
+	        }
+        	w.WriteHeader(http.StatusOK)
+        	w.Write([]byte("OK"))
+    	})
+
 	fmt.Println("Server is listening on port 8888...")
 	log.Fatal(http.ListenAndServe(":8888", nil))
 
